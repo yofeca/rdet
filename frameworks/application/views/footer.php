b78e66ef1010ef6b6df63e8cfefc682f49c2c4d0
Jun Sonson <sirex.jbs@gmail.com>
2014-03-24 14:31:34 +0800

    Linux try commit

--- frameworks/application/views/footer.php
+++ frameworks/application/views/footer.php
@@ -2,7 +2,7 @@
 <div id="panel-bottom">
         <p>&COPY; 2013 All Rights Reserved.<p>
         <p>Research and Development, Extension and Training (RDET)</p>
-        <p>Ifugao State University</p>
+        <p><strong>Ifugao State University</strong></p>
     </div>
 </body>
 </html>
